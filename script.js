4a17c1c6dfbdd3d030b5d63da2e7440a5cd29dac
Dhanushvel123 <vdhanush480@gmail.com>
2025-03-18 12:40:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Dental website loaded successfully!");
 });
+
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("show");
+}
+
+function toggleDropdown() {
+    document.querySelector(".dropdown-menu").classList.toggle("show");
+}
